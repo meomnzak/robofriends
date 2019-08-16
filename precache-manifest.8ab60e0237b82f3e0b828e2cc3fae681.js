abf8aef42656fc1041ebc7857e5f9ef8496b80c2
moemnzak <brainiac879@gmail.com>
2019-08-16 22:23:18 +0200

    Updates

---
+++ precache-manifest.8ab60e0237b82f3e0b828e2cc3fae681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a098488406890415923e4c5ecbad6571",
+    "url": "/meomnzak/robofriends.git/index.html"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/meomnzak/robofriends.git/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e6b243c98ba134172937",
+    "url": "/meomnzak/robofriends.git/static/css/main.5aa04dc7.chunk.css"
+  },
+  {
+    "revision": "200c6d7eecdb7727964e",
+    "url": "/meomnzak/robofriends.git/static/js/2.02ef720e.chunk.js"
+  },
+  {
+    "revision": "e6b243c98ba134172937",
+    "url": "/meomnzak/robofriends.git/static/js/main.d45c5e9d.chunk.js"
+  },
+  {
+    "revision": "7f2c3189facc79acbbc0",
+    "url": "/meomnzak/robofriends.git/static/js/runtime~main.7bd7934f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/meomnzak/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
